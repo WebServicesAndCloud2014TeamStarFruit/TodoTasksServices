4e07a4dd0ee421cf69078b713f0c295f5f1a8cf6
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-09-17 16:01:05 +0300

    added ApplicationUser and OData support

--- TodoTasks.Services/App_Start/WebApiConfig.cs
+++ TodoTasks.Services/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.OData.Extensions;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -20,7 +21,7 @@ namespace TodoTasks.Services
             // Web API routes
             config.MapHttpAttributeRoutes();
             config.EnableCors();
-            //config.AddODataQueryFilter();
+            config.AddODataQueryFilter();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
