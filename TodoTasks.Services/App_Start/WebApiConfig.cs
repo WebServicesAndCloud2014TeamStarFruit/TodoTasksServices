1f2cedce4469b6fe1b147202307df5a34985dbd9
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2014-09-17 22:55:51 +0300

    cors for specific domain

--- TodoTasks.Services/App_Start/WebApiConfig.cs
+++ TodoTasks.Services/App_Start/WebApiConfig.cs
@@ -1,34 +1,35 @@
-﻿using System;
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.OData.Extensions;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 using System.Web.Http.Cors;
+using System.Web.OData.Extensions;
 
 namespace TodoTasks.Services
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			// Configure Web API to use only bearer token authentication.
+			config.SuppressDefaultHostAuthentication();
+			config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
-            config.AddODataQueryFilter();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			var corsAttribute = new EnableCorsAttribute("http://localhost:11615", "*", "*");
+			config.EnableCors(corsAttribute);
+
+			config.AddODataQueryFilter();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
