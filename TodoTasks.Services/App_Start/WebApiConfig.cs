5adcec583dbb578bd084c7254ac31c6b130603a8
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-09-17 10:55:30 +0300

    bug fix odata

--- TodoTasks.Services/App_Start/WebApiConfig.cs
+++ TodoTasks.Services/App_Start/WebApiConfig.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.OData.Extensions;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -19,7 +18,7 @@ namespace TodoTasks.Services
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             config.EnableCors();
-            config.AddODataQueryFilter();
+            //config.AddODataQueryFilter();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
