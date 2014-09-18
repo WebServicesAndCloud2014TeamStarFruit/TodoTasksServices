bbd490a333320b5aa9a281e2399eb0e157bbe535
georgipyaramov <georgipyaramov@gmail.com>
2014-09-18 11:32:14 +0300

    Added key for accessig the google drive service

--- TodoTasks.FileExporter/GoogleDriveFileExporter.cs
+++ TodoTasks.FileExporter/GoogleDriveFileExporter.cs
@@ -12,8 +12,9 @@
 
     public class GoogleDriveFileExporter
     {
-        private const string SERVICE_ACCOUNT_PKCS12_FILE_PATH = @".\";
+        private const string SERVICE_ACCOUNT_PKCS12_FILE_PATH = @".\My Project-69d8888d8990.p12";
         private const string SERVICE_ACCOUNT_EMAIL = @"699545407731-a12caj72eb9hj9mafgckndk2l5dpo1av@developer.gserviceaccount.com";
+        
         static DriveService BuildService()
         {
             X509Certificate2 certificate = new X509Certificate2(SERVICE_ACCOUNT_PKCS12_FILE_PATH, "notasecret", X509KeyStorageFlags.Exportable);
@@ -21,14 +22,14 @@
             ServiceAccountCredential credential = new ServiceAccountCredential(
                 new ServiceAccountCredential.Initializer(SERVICE_ACCOUNT_EMAIL)
                 {
-                    User = "someone@mydomain.mygbiz.com",
+                    User = "georgipyaramov@gmail.com",
                     Scopes = new[] { DriveService.Scope.DriveFile }
                 }.FromCertificate(certificate));
 
             var service = new DriveService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = "Drive API Sample",
+                ApplicationName = "Drive API",
             });
 
             return service;
