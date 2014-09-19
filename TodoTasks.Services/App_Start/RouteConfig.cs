adc7997f600bd073a2775cbe442d55c24331f81a
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2014-09-19 12:56:01 +0300

    Web Cleanup again

--- TodoTasks.Services/App_Start/RouteConfig.cs
+++ TodoTasks.Services/App_Start/RouteConfig.cs
@@ -12,17 +12,11 @@ namespace TodoTasks.Services
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			//routes.MapRoute(
-			//	name: "Default",
-			//	url: "{controller}/{action}/{id}",
-			//	defaults: new { controller = "Help", action = "Index", id = UrlParameter.Optional }
-			//).DataTokens = new RouteValueDictionary(new { area = "HelpPage" });
-
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
+				defaults: new { controller = "Help", action = "Index", id = UrlParameter.Optional }
+			).DataTokens = new RouteValueDictionary(new { area = "HelpPage" });
 		}
 	}
 }
